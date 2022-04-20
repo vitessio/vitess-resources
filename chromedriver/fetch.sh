09a6c769718226075a4ca275b90deb5f5b895182
Rameez Sajwani <rameezwazirali@hotmail.com>
2022-04-20 11:13:31 -0700

    Revert to chromedrive v6.0.3
    Signed-off-by: Rameez Sajwani <rameezwazirali@hotmail.com>

--- chromedriver/fetch.sh
+++ chromedriver/fetch.sh
@@ -1,4 +1,3 @@
-https://github.com/electron/electron/releases/download/v18.0.4/chromedriver-v18.0.4-darwin-arm64.zip
-https://github.com/electron/electron/releases/download/v18.0.4/chromedriver-v18.0.4-darwin-x64.zip
-https://github.com/electron/electron/releases/download/v18.0.4/chromedriver-v18.0.4-linux-arm64.zip
-https://github.com/electron/electron/releases/download/v18.0.4/chromedriver-v18.0.4-linux-x64.zip
+https://github.com/electron/electron/releases/download/v6.0.3/chromedriver-v6.0.3-darwin-x64.zip
+https://github.com/electron/electron/releases/download/v6.0.3/chromedriver-v6.0.3-linux-x64.zip
+https://github.com/electron/electron/releases/download/v6.0.3/chromedriver-v6.0.3-linux-arm64.zip
