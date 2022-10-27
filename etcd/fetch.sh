5190022d05a62b5eabb0cdbb507cf66ee61c1d51
Iheanyi Ekechukwu <iekechukwu@gmail.com>
2022-10-27 14:39:29 -0500

    Update fetch.sh script.

--- etcd/fetch.sh
+++ etcd/fetch.sh
@@ -1,4 +1,4 @@
-https://github.com/etcd-io/etcd/releases/download/v3.5.3/etcd-v3.5.3-linux-amd64.tar.gz
-https://github.com/etcd-io/etcd/releases/download/v3.5.3/etcd-v3.5.3-linux-arm64.tar.gz
-https://github.com/etcd-io/etcd/releases/download/v3.5.3/etcd-v3.5.3-darwin-amd64.zip
+https://github.com/etcd-io/etcd/releases/download/v3.5.5/etcd-v3.5.5-linux-amd64.tar.gz
+https://github.com/etcd-io/etcd/releases/download/v3.5.5/etcd-v3.5.5-linux-arm64.tar.gz
+https://github.com/etcd-io/etcd/releases/download/v3.5.5/etcd-v3.5.5-darwin-amd64.zip
 https://github.com/etcd-io/etcd/releases/download/v3.5.5/etcd-v3.5.5-darwin-arm64.zip
