6741b8d80143e71155b18177a58ae91fdb202c4b
Florent Poinsard <florent.poinsard@outlook.fr>
2023-02-08 11:51:37 +0100

    Remove zip from mariadb folder and add 10.6.12
    Signed-off-by: Florent Poinsard <florent.poinsard@outlook.fr>

--- mariadb/fetch.sh
+++ mariadb/fetch.sh
@@ -1,14 +1,2 @@
-#!/bin/bash
-
-wget https://mirror.rackspace.com/mariadb//mariadb-10.10.3/bintar-linux-systemd-x86_64/mariadb-10.10.3-linux-systemd-x86_64.tar.gz
-
-tar -xvzf mariadb-10.10.3-linux-systemd-x86_64.tar.gz
-
-mkdir mariadb-10.10.3-linux-x86_64
-
-mv mariadb-10.10.3-linux-systemd-x86_64/bin/mariadb mariadb-10.10.3-linux-x86_64/mariadb
-
-zip mariadb-10.10.3-linux-x86_64.zip mariadb-10.10.3-linux-x86_64/mariadb
-
-rm -Rf mariadb-10.10.3-linux-systemd-x86_64.tar.gz mariadb-10.10.3-linux-systemd-x86_64 mariadb-10.10.3-linux-x86_64
-
+https://mirror.rackspace.com/mariadb//mariadb-10.10.3/bintar-linux-systemd-x86_64/mariadb-10.10.3-linux-systemd-x86_64.tar.gz
+https://mirror.rackspace.com/mariadb//mariadb-10.6.12/bintar-linux-systemd-x86_64/mariadb-10.6.12-linux-systemd-x86_64.tar.gz
